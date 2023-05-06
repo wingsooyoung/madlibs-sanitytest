8b35f37a68541f8090eb8e852d5890a81c09afaf
wingsooyoung <hsm_rox_16@hotmail.com>
2023-05-05 21:06:31 -0400

    Update schema.js

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,24 +1,14 @@
-// First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
-
-// Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 import madlib from './madlib'
-import madlibField from './objects/madLibField'
+import madLibField from './objects/madLibField'
 import userLib from './userLib'
 
 // Then we give our schema to the builder and provide the result to Sanity
-export default createSchema({
-  // We name our schema
-  name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
+export default [
     // document
     madlib,
     userLib,
     //objects
-    madlibField
-  ])
-})
+    madLibField  
+]
+
