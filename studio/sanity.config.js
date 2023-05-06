4b55f580250715efa4c3005109ea64e6f9a46092
wingsooyoung <hsm_rox_16@hotmail.com>
2023-05-05 21:04:26 -0400

    Update sanity.config.js

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -1,11 +1,12 @@
 // sanity.config.js
 import { defineConfig } from "sanity";
+import { deskTool } from 'sanity/desk'
 
 export default defineConfig({
   title: "brown-pony",
   projectId: "t2qgvbba",
   dataset: "production",
-  plugins: [],
+  plugins: [deskTool()],
   schema: {
     types: [],
   },
