d82b63ba31808c5b953dc7e40337bbfc6690ceec
wingsooyoung <hsm_rox_16@hotmail.com>
2023-05-05 21:29:13 -0400

    Update sanityClient.js

--- site/utils/sanityClient.js
+++ site/utils/sanityClient.js
@@ -1,9 +1,13 @@
-const sanityClient = require('@sanity/client')
-module.exports = sanityClient({
-    projectId: 'eyrp0gsd',
-    dataset: 'production',
-    apiVersion: '2021-06-07',
-    useCdn: true
+const {createClient} = require('@sanity/client')
 
+const client = createClient({
+  projectId: 't2qgvbba',
+  dataset: 'production',
+  useCdn: true, // set to `false` to bypass the edge cache
+  apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
 })
 
+client
+  .fetch(`count(*)`)
+  .then((data) => console.log(`Number of documents: ${data}`))
+  .catch(console.error)
