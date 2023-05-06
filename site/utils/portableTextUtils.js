ca2ecb348ac7378adaaf560ff81c78ef25775983
wingsooyoung <hsm_rox_16@hotmail.com>
2023-05-06 19:52:42 +0000

    hiiiiiii

--- site/utils/portableTextUtils.js
+++ site/utils/portableTextUtils.js
@@ -1,27 +1,35 @@
-import htm from 'htm'
-import vhtml from 'vhtml'
-import {toHTML} from '@portabletext/to-html'
+const {htm} = require('htm');
+const {toHTML} =  require('@portabletext/to-html');
 
-const html = htm.bind(vhtml)
+function h(type, props, ...children) {
+  return { type, props, children };
+}
+const html = htm.bind(h);
+
+console.log( html`<h1 id=hello>Hello world!</h1>` );
 
 const myPortableTextComponents = {
   types: {
-        madLibField: ({value}) => html`<span class="empty" id="${value._key}">${value.displayText}</span>`
+    madLibField: ({
+      value
+    }) => html`<span class="empty" id="${value._key}">${value.displayText}</span>`
   }
-}
+};
+const prepText = data => {
+  // Takes the data from a specific Sanity document
+  // and creates a new htmlText property to contain the HTML
+  // This lets us keep the Portable Text data intact and still display HTML
 
-
-const prepText = (data) => {
-    // Takes the data from a specific Sanity document
-    // and creates a new htmlText property to contain the HTML
-    // This lets us keep the Portable Text data intact and still display HTML
-  
-    return {
-        ...data,
-        htmlText: toHTML(data.text, {components: myPortableTextComponents})
-    }
-}
+  return {
+    ...data,
+    htmlText: toHTML(data.text, {
+      components: myPortableTextComponents
+    })
+  };
+};
 
 // We only need to export prepText for our functions
 
-module.exports = {prepText}
+module.exports = {
+  prepText
+};
