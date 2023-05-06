13dc646d78aa11460226e014898b2bab15ff9864
wingsooyoung <hsm_rox_16@hotmail.com>
2023-05-06 05:46:05 +0000

    commit

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -1,39 +1,42 @@
 // sanity.config.js
-import { defineConfig } from "sanity";
-import { deskTool } from 'sanity/desk';
-import schemas from './schemas/schema';
-import { visionTool } from '@sanity/vision';
+const {defineConfig} = require("sanity");
+const {deskTool} = require('sanity/desk');
+const schemas = require('./schemas/schema');
+const {visionTool} = require('@sanity/vision');
 
-export default defineConfig({
+module.exports = defineConfig({
   title: "brown-pony",
   projectId: "t2qgvbba",
   dataset: "production",
-  plugins: [
-    deskTool(),
-    visionTool()
-  ],
-  tools: (prev) => {
+  plugins: [deskTool(), visionTool()],
+  tools: prev => {
     // 👇 Uses environment variables set by Vite in development mode
     if (import.meta.env.DEV) {
-      return prev
+      return prev;
     }
-    return prev.filter((tool) => tool.name !== 'vision')
+    return prev.filter(tool => tool.name !== 'vision');
   },
   schema: {
-    types: schemas,
+    types: schemas
   },
   document: {
-    newDocumentOptions: (prev, { creationContext }) => {
+    newDocumentOptions: (prev, {
+      creationContext
+    }) => {
       if (creationContext.type === 'global') {
-        return prev.filter((templateItem) => templateItem.templateId != 'settings')
+        return prev.filter(templateItem => templateItem.templateId != 'settings');
       }
-      return prev
+      return prev;
     },
-    actions: (prev, { schemaType }) => {
+    actions: (prev, {
+      schemaType
+    }) => {
       if (schemaType === 'settings') {
-        return prev.filter(({ action }) => !['unpublish', 'delete','duplicate'].includes(action))
+        return prev.filter(({
+          action
+        }) => !['unpublish', 'delete', 'duplicate'].includes(action));
       }
-      return prev
-    },
-  },
+      return prev;
+    }
+  }
 });
