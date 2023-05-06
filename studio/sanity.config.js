73537315a68a5a3ba012b54e43b28af7412c231a
wingsooyoung <hsm_rox_16@hotmail.com>
2023-05-05 21:02:05 -0400

    Create sanity.config.js

---
+++ studio/sanity.config.js
@@ -0,0 +1,12 @@
+// sanity.config.js
+import { defineConfig } from "sanity";
+
+export default defineConfig({
+  title: "brown-pony",
+  projectId: "t2qgvbba",
+  dataset: "production",
+  plugins: [],
+  schema: {
+    types: [],
+  },
+});
