a64c6e168d023c2a46f383ea7b6fde2578f82768
wingsooyoung <hsm_rox_16@hotmail.com>
2023-05-07 20:57:48 -0400

    commit #3.2 on the sanity studio side of "rollback from square 1"! - will esbuild save my serverless function? + bundler settings i commented out ARE important. maybe.

--- site/.eleventy.js
+++ site/.eleventy.js
@@ -5,8 +5,8 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
     name: "userlibs", // the name to use for the functions
-    // functionsDir: "./functions/", // The functions directory
+    functionsDir: "./functions/", // The functions directory
     copy: ["utils/"], // Any files that need to be copied to make our scripts work
-    // excludeDependencies: ["./_data/madlibs.js"] // Exclude any files you don't want to run
+    excludeDependencies: ["./_data/madlibs.js"] // Exclude any files you don't want to run
   });
 };
