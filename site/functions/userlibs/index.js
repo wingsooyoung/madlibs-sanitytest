6ccd87f5fd6b135aea3ef7211c053d92760f1749
wingsooyoung <hsm_rox_16@hotmail.com>
2023-05-14 09:22:46 -0400

    commit #3.4 on the site side of "rollback from square 1"! - adjusting

--- site/functions/userlibs/index.js
+++ site/functions/userlibs/index.js
@@ -22,6 +22,23 @@ async function handler(event) {
     }
   });
 
+
+  try {
+    // returns the HTML for the Eleventy template that matches to the URL
+    // Can use with `eleventyConfig.dataFilterSelectors` to put data cascade data into `page.data` here.
+    let [page] = await elev.getOutput();
+    let html = page.content;
+
+    return {
+      statusCode: 200,
+      body: html
+    };
+  } catch(e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: e.message })
+    };
+  }
   try {
     return {
       statusCode: 200,
